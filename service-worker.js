86d462dd554129d31aecc67d9a0e7340d1d65233
CREED-DFX <atomtoo57@gmail.com>
2025-07-30 06:01:15 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('bmr-cache').then(cache => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './style.css',
+                './main.js',
+                './manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
